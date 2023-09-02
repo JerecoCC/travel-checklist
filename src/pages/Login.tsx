83e63afeed17ba1d24e7599e64fc95c8260fca52
Jereco Catingub <jerecocatingub216@gmail.com>
2023-09-02 20:09:29 +0800

    Added logic for Facebook and Google OAuth login.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,28 @@
 import React, { FC } from "react";
 import { Button, Divider, IconButton, Input, Text } from "@chakra-ui/react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGoogle, faFacebook, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faGoogle, faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { supabase } from "../lib/helper/supabaseClient";
+import { Provider } from "@supabase/supabase-js";
 
 export const Login: FC = () => {
+  const loginWithSocMed = async (provider: Provider) => {
+    try {
+      const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: provider,
+        options: {
+          queryParams: {
+            access_type: "offline",
+            prompt: "consent",
+          },
+        },
+      });
+      if (error) throw error;
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
   return (
     <div className="flex h-full">
       <section className="w-2/3 flex flex-col justify-center items-center gap-4 px-20 text-center">
@@ -13,11 +32,13 @@ export const Login: FC = () => {
             focusBorderColor="teal.400"
             placeholder="Email"
             width="full"
+            autoComplete="email"
           />
           <Input
             focusBorderColor="teal.400"
             placeholder="Password"
             type="password"
+            autoComplete="current-password"
           />
           <Button
             colorScheme="teal"
@@ -26,31 +47,28 @@ export const Login: FC = () => {
             Sign In
           </Button>
         </form>
-        <Divider className="my-6"/>
+        <Divider className="my-6" width="xl" />
         <div>
-          <Text fontSize="xl" color="gray.500" className="mb-4">Login using social networks</Text>
+          <Text fontSize="lg" color="gray.400" className="mb-4">Login using social networks</Text>
           <div className="flex justify-center gap-4">
             <IconButton
               colorScheme="facebook"
               aria-label="Sign In Facebook"
               icon={<FontAwesomeIcon icon={faFacebook} />}
+              onClick={() => loginWithSocMed("facebook")}
             />
             <IconButton
               colorScheme="red"
               aria-label="Sign In Google"
               icon={<FontAwesomeIcon icon={faGoogle} />}
-            />
-            <IconButton
-              colorScheme="twitter"
-              aria-label="Sign In Twitter"
-              icon={<FontAwesomeIcon icon={faTwitter} />}
+              onClick={() => loginWithSocMed("google")}
             />
           </div>
         </div>
       </section>
       <section className="w-1/3 flex flex-col justify-center items-center gap-4 bg-teal-500 text-white text-center px-16">
         <Text fontSize="5xl" as="b">New Here?</Text>
-        <Text fontSize="xl">Sign up now to start creating checklists</Text>
+        <Text fontSize="lg">Sign up now to start creating checklists</Text>
         <Button
           colorScheme="whiteAlpha"
           color="ButtonText"
